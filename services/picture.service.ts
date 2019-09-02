8d6d1c90eec195ae55333d928710ed10a20102fd
BlandineK <blandineuwase222@gmail.com>
2019-09-02 17:43:26 +0200

    add new imports

--- services/picture.service.ts
+++ services/picture.service.ts
@@ -5,14 +5,14 @@ import { HttpClient} from '@angular/common/http';
 })
 export class PictureService {
   private username:string;
-  private clientid:'459c220d37570c29aa28';
-  private clientsecret:'dd02d8b3ce547564cb08903dce92a466e18c8b63';
+  private clientid='459c220d37570c29aa28';
+  private clientsecret='dd02d8b3ce547564cb08903dce92a466e18c8b63';
   
   constructor(private http:HttpClient) { 
     console.log("The app is now ready");
     this.username = 'BlandineK';
   }
   getProfileInfo(){
-    return this.http.get ("https://api.github.com/users/BlandineK"); 
+    return this.http.get ("https://api.github.com/users/BlandineK" + "client_id= " + this.clientid + "&client_secret=" + this.clientsecret); 
   }
 }
