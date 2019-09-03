0b1fdfcf0c453046423bc4c69b3772dcab912f69
BlandineK <blandineuwase222@gmail.com>
2019-09-03 11:04:18 +0200

    add bootstrap

--- src/app/picture/picture.component.ts
+++ src/app/picture/picture.component.ts
@@ -7,9 +7,11 @@ import { PictureService } from 'services/picture.service';
   styleUrls: ['./picture.component.css']
 })
 export class PictureComponent implements OnInit {
-
+ picture ; 
   constructor(private pictureService: PictureService) { 
      this.pictureService.getProfileInfo().subscribe(picture => {console.log(picture);
+     this.picture = picture;
+     
     });
   }
   ngOnInit() {
